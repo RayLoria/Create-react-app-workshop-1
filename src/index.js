431ed3f9b3d8364d83f5c67cbd2751d43bffeaf7
Ray Loria <raygor_0518@hotmail.comm>
2022-02-15 23:33:34 -0600

    workshop 1 completed

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!!!</h1>
+      </div>
+    );
+  }
+ }
+  
+ ReactDOM.render(<App/>, document.getElementById('root'));
